7d578f5695553d5e4dd26e7056cc83e74af4395f
_ <153122375+qian-cheng-awa@users.noreply.github.com>
2025-02-15 07:42:43 +0800

    Update script.lua

--- script.lua
+++ script.lua
@@ -13,11 +13,15 @@ local Window = Rayfield:CreateWindow({
 local Tab = Window:CreateTab("主要功能", 4483362458)
 
 local item = nil
-
+local items = {}
 local function updataitem()
     local tb = {}
-    for i,v in ipairs(workspace:WaitForChild("RuntimeItems"):GetChildren()) do
-        tb[i] = tostring(i).." "..tostring(v.Name)
+    items = {}
+    for i,v in ipairs(workspace:GetDescendants()) do
+        if v:FindFirstChild("ObjectInfo") then
+            tb[#tb + 1] = tostring(i).." "..tostring(v.Name)
+            items[tostring(i).." "..tostring(v.Name)] = v
+        end
     end
     return tb
 end
@@ -31,7 +35,7 @@ local Dropdown = Tab:CreateDropdown({
     Callback = function(Options)
         -- The function that takes place when the selected option is changed
         -- The variable (Options) is a table of strings for the current selected options
-        item = workspace:WaitForChild("RuntimeItems"):GetChildren()[tonumber(string.sub(unpack(Options),0,string.find(unpack(Options)," ")))]
+        item = items[unpack(Options)]
     end,
 })
 
@@ -70,43 +74,6 @@ local v = false
 local Toggle = Tab:CreateToggle({
     Name = "物品透视",
     CurrentValue = false,
-    Flag = "Toggle1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
-    Callback = function(Value)
-        v = Value
-    end,
-})
-
-game:GetService("RunService").RenderStepped:Connect(function()
-    for i,itemi in ipairs(workspace:WaitForChild("RuntimeItems"):GetChildren()) do
-        if v then
-            if not itemi:GetAttribute("ESP") then
-                itemi:SetAttribute("ESP",true)
-                local bill = Instance.new("BillboardGui")
-                bill.Name = "Espui"
-                bill.Parent = itemi
-                bill.Adornee = itemi
-                bill.Size = UDim2.new(10,0,5,0)
-                bill.AlwaysOnTop = true
-                local textlabel = Instance.new("TextLabel")
-                textlabel.Parent = bill
-                textlabel.Size = UDim2.new(1,0,1,0)
-                textlabel.BackgroundTransparency = 1
-                textlabel.Text = itemi.Name
-                textlabel.TextColor3 = Color3.new(1,1,1)
-                textlabel.TextScaled = true
-                local stk = Instance.new("UIStroke")
-                stk.Parent = textlabel
-                stk.Thickness = 2
-                stk.LineJoinMode = Enum.LineJoinMode.Miter
-            end
-        else
-            if itemi:GetAttribute("ESP") then
-                itemi:SetAttribute("ESP",false)
-                itemi:FindFirstChild("Espui"):Destroy()
-            end
-        end
-    end
-end)    CurrentValue = false,
     Flag = "Toggle1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
     Callback = function(Value)
         v = Value
