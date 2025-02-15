0284c6fa085d83fc863987d2bd2a758e32068581
_ <153122375+qian-cheng-awa@users.noreply.github.com>
2025-02-15 22:09:36 +0800

    Update script.lua

--- script.lua
+++ script.lua
@@ -115,48 +115,4 @@ game:GetService("RunService").RenderStepped:Connect(function()
             end
         end
     end
-end)})
-
-
-local v = false
-
-local Toggle = Tab:CreateToggle({
-    Name = "物品透视",
-    CurrentValue = false,
-    Flag = "Toggle1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
-    Callback = function(Value)
-        v = Value
-    end,
-})
-
-game:GetService("RunService").RenderStepped:Connect(function()
-    for i,itemi in ipairs(workspace:WaitForChild("RuntimeItems"):GetChildren()) do
-        if v then
-            if not itemi:GetAttribute("ESP") then
-                itemi:SetAttribute("ESP",true)
-                local bill = Instance.new("BillboardGui")
-                bill.Name = "Espui"
-                bill.Parent = itemi
-                bill.Adornee = itemi
-                bill.Size = UDim2.new(10,0,5,0)
-                bill.AlwaysOnTop = true
-                local textlabel = Instance.new("TextLabel")
-                textlabel.Parent = bill
-                textlabel.Size = UDim2.new(1,0,1,0)
-                textlabel.BackgroundTransparency = 1
-                textlabel.Text = itemi.Name
-                textlabel.TextColor3 = Color3.new(1,1,1)
-                textlabel.TextScaled = true
-                local stk = Instance.new("UIStroke")
-                stk.Parent = textlabel
-                stk.Thickness = 2
-                stk.LineJoinMode = Enum.LineJoinMode.Miter
-            end
-        else
-            if itemi:GetAttribute("ESP") then
-                itemi:SetAttribute("ESP",false)
-                itemi:FindFirstChild("Espui"):Destroy()
-            end
-        end
-    end
 end)
